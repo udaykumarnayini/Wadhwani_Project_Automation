22137e47495a6aede18546e3eea2da66accc5da5
DESKTOP-K08KNER\User <udaykumar.nayini@qapitol.com>
2021-12-08 19:33:07 +0530

    Added aventstack reports

--- src/test/java/com/run/Runner.java
+++ src/test/java/com/run/Runner.java
@@ -16,10 +16,12 @@ import cucumber.api.junit.Cucumber;
 
         // To run Faculty Cases//
 @CucumberOptions(features= {"src/test/java/feature/faculty.feature"},glue= {"com.step"},
-tags= {"@tag1,@tag2,@tag3,@tag4,@tag5,@tag6,@tag7,@tag8,@tag9,@tag10,@tag11,@tag12,@tag13,@tag14,@tag15,@tag16,@tag17,@tag18,@tag19,@tag20,@tag21,@tag22,@tag23,@tag24,@tag25,@tag26,@tag27,@tag28,@tag29,@tag30,@tag31,@tag32,@tag33,@tag34,@tag35,@tag36,@tag37,@tag38,@tag39,@tag40,@tag41,@tag42,@tag43,@tag44,@tag45,@tag46,@tag47,@tag48,@tag49,@tag50,@tag51,@tag52,@tag53,@tag54,@tag55"},
+//tags= {"@tag1,@tag2,@tag3,@tag4,@tag5,@tag6,@tag7,@tag8,@tag9,@tag10,@tag11,@tag12,@tag13,@tag14,@tag15,@tag16,@tag17,@tag18,@tag19,@tag20,@tag21,@tag22,@tag23,@tag24,@tag25,@tag26,@tag27,@tag28,@tag29,@tag30,@tag31,@tag32,@tag33,@tag34,@tag35,@tag36,@tag37,@tag38,@tag39,@tag40,@tag41,@tag42,@tag43,@tag44,@tag45,@tag46,@tag47,@tag48,@tag49,@tag50,@tag51,@tag52,@tag53,@tag54,@tag55"},
+tags={"@tag3,@tag4"},
 
 monochrome=true,dryRun=false,snippets = SnippetType.CAMELCASE,
-plugin = {"pretty","html:test-output"})
+plugin = {"pretty","html:test-output",
+          "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
 public class Runner {
 
